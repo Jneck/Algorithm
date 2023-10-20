d74c7e2bada51fbbf8d7a89367a48d5459d6e7f8
ChanYoung <91073404+Jneck@users.noreply.github.com>
2023-10-20 19:13:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,14 @@
+N, K =map(int,input().split())
+D = [[1 for i in range(N+1)]for i in range(N+1)]
+
+# 리스트 초기화
+for i in range(1, N+1):
+    D[i][1] = i
+    D[i][i] = 1
+    D[i][0] = 1
+
+for i in range(3, N+1):
+    for j in range(2, i):
+        D[i][j] = D[i-1][j] + D[i-1][j-1]
+
+print(D[N][K])
