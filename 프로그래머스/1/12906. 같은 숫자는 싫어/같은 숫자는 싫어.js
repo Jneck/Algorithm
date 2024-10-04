bf504c0feaf589a1face91b7fd4c8f131d41412f
ChanYoung <91073404+Jneck@users.noreply.github.com>
2024-10-04 20:53:55 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.83 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr)
+{
+    var answer = [];
+    
+    for (const number of arr) {
+        if (answer[answer.length-1] != number) {
+            answer.push(number)
+        }
+    }
+
+    return answer;
+}
