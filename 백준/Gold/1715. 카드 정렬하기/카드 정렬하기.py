d78b29038888bb07f0f1b1d77ffc897850f31fb0
ChanYoung <91073404+Jneck@users.noreply.github.com>
2025-04-01 16:47:19 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 524 ms, Memory: 40540 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,18 +1,24 @@
 import sys
-
 input = sys.stdin.readline
-N = int(input())
 
 from queue import PriorityQueue
-
 pq = PriorityQueue()
-answer = 0
-for i in range(N):
+
+
+N = int(input())
+for _ in range(N):
     pq.put(int(input()))
 
-while 1 != pq.qsize():
-    new_card_bucket = pq.get() + pq.get()
-    answer += new_card_bucket
-    pq.put(new_card_bucket)
+answer = 0
+while True:
+    min_size = pq.get()
+    if pq.empty():
+        break
+    second_min_size = pq.get()
+
+    new_size = min_size + second_min_size
+    answer += new_size
+    
+    pq.put(new_size)
 
 print(answer)
